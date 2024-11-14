748f5697c7c0d3724a6d23ae564586ffa3111f3b
Florian Friedrich <ffried@me.com>
2024-11-14 12:10:46 +0100

    Improve handling of invalid images

--- internal/kubehelpers/imgs-in-use.go
+++ internal/kubehelpers/imgs-in-use.go
@@ -2,6 +2,7 @@ package kubehelpers
 
 import (
 	"context"
+	"errors"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -16,63 +17,93 @@ type DockerImage struct {
 	Tag      string
 }
 
-func imagesInPods(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]string, error) {
+type imageSpec struct {
+	Image   string
+	IsValid bool
+}
+
+func appendImagesInUseInSpec(specSource string, imagesInUse *[]imageSpec, podSpec corev1.PodSpec) {
+	for _, container := range append(podSpec.Containers, podSpec.InitContainers...) {
+		isValid := len(container.Image) > 0
+		if !isValid {
+			println("Found invalid image in container " + container.Name + " of " + specSource + ": " + container.Image)
+		}
+		*imagesInUse = append(*imagesInUse, imageSpec{container.Image, isValid})
+	}
+	for _, container := range podSpec.EphemeralContainers {
+		isValid := len(container.Image) > 0
+		if !isValid {
+			println("Found invalid image in ephemeral container " + container.Name + " of " + specSource + ": " + container.Image)
+		}
+		*imagesInUse = append(*imagesInUse, imageSpec{container.Image, isValid})
+	}
+}
+
+func imagesInPods(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]imageSpec, error) {
 	list, err := clients.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
-	imagesInUse := make([]string, 0, len(list.Items))
+	imagesInUse := make([]imageSpec, 0, len(list.Items))
 	for _, pod := range list.Items {
 		for _, status := range append(pod.Status.ContainerStatuses, append(pod.Status.InitContainerStatuses, pod.Status.EphemeralContainerStatuses...)...) {
 			if status.Ready || (status.Started != nil && *status.Started) || status.State.Running != nil || status.State.Waiting != nil {
 				if len(status.ImageID) == 0 {
-					println("Found invalid image in pod " + pod.Name + " with status " + status.State.String() + ": " + status.ImageID)
-					//continue
+					println("Found invalid (empty) image in pod " + pod.Name + " with status " + status.State.String() + ": " + status.ImageID + ". Checking spec...")
+					appendImagesInUseInSpec("pod "+pod.Name+" ", &imagesInUse, pod.Spec)
+				} else {
+					imagesInUse = append(imagesInUse, imageSpec{status.ImageID, true})
 				}
-				imagesInUse = append(imagesInUse, status.ImageID)
 			}
 		}
 	}
 	return imagesInUse, nil
 }
 
-func appendImagesInUseInSpec(specSource string, imagesInUse *[]string, podSpec corev1.PodSpec) {
-	for _, container := range append(podSpec.Containers, podSpec.InitContainers...) {
-		if len(container.Image) == 0 {
-			println("Found invalid image in container " + container.Name + " of " + specSource + ": " + container.Image)
-			//continue
-		}
-		*imagesInUse = append(*imagesInUse, container.Image)
-	}
-	for _, container := range podSpec.EphemeralContainers {
-		if len(container.Image) == 0 {
-			println("Found invalid image in ephemeral container " + container.Name + " of " + specSource + ": " + container.Image)
-			//continue
-		}
-		*imagesInUse = append(*imagesInUse, container.Image)
-	}
-}
-
-func imagesInCronJobs(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]string, error) {
+func imagesInCronJobs(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]imageSpec, error) {
 	list, err := clients.BatchV1().CronJobs(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
-	imagesInUse := make([]string, 0, len(list.Items))
+	imagesInUse := make([]imageSpec, 0, len(list.Items))
 	for _, job := range list.Items {
 		appendImagesInUseInSpec("cron job "+job.Name+" ", &imagesInUse, job.Spec.JobTemplate.Spec.Template.Spec)
 	}
 	return imagesInUse, nil
 }
 
-func imagesInDeployments(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]string, error) {
+func imagesInJobs(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]imageSpec, error) {
+	list, err := clients.BatchV1().Jobs(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	imagesInUse := make([]imageSpec, 0, len(list.Items))
+	for _, job := range list.Items {
+		appendImagesInUseInSpec("job "+job.Name+" ", &imagesInUse, job.Spec.Template.Spec)
+	}
+	return imagesInUse, nil
+}
+
+func imagesInDeployments(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]imageSpec, error) {
 	list, err := clients.AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
-	imagesInUse := make([]string, 0, len(list.Items))
+	imagesInUse := make([]imageSpec, 0, len(list.Items))
 	for _, deployment := range list.Items {
 		appendImagesInUseInSpec("deployment "+deployment.Name+" ", &imagesInUse, deployment.Spec.Template.Spec)
+	}
+	return imagesInUse, nil
+}
+
+func imagesInStatefulSets(ctx context.Context, clients *kubernetes.Clientset, namespace string) ([]imageSpec, error) {
+	list, err := clients.AppsV1().StatefulSets(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	imagesInUse := make([]imageSpec, 0, len(list.Items))
+	for _, statefulSet := range list.Items {
+		appendImagesInUseInSpec("stateful set "+statefulSet.Name+" ", &imagesInUse, statefulSet.Spec.Template.Spec)
 	}
 	return imagesInUse, nil
 }
@@ -88,7 +119,7 @@ func FindImagesInUse(ctx context.Context, config Config) ([]DockerImage, error) 
 		return nil, err
 	}
 
-	outputChannel := make(chan string)
+	outputChannel := make(chan imageSpec)
 	errChannel := make(chan error, 1)
 	sendError := func(errorChannel chan<- error, err error) {
 		select {
@@ -98,7 +129,7 @@ func FindImagesInUse(ctx context.Context, config Config) ([]DockerImage, error) 
 		}
 	}
 	var wg sync.WaitGroup
-	runJob := func(job func() ([]string, error)) {
+	runJob := func(job func() ([]imageSpec, error)) {
 		wg.Add(1)
 		go func(errorChannel chan<- error) {
 			defer wg.Done()
@@ -112,31 +143,45 @@ func FindImagesInUse(ctx context.Context, config Config) ([]DockerImage, error) 
 			}
 		}(errChannel)
 	}
-	runJob(func() ([]string, error) {
+	runJob(func() ([]imageSpec, error) {
 		return imagesInPods(ctx, clients, config.Namespace)
 	})
-	runJob(func() ([]string, error) {
+	runJob(func() ([]imageSpec, error) {
 		return imagesInCronJobs(ctx, clients, config.Namespace)
 	})
-	runJob(func() ([]string, error) {
+	runJob(func() ([]imageSpec, error) {
+		return imagesInJobs(ctx, clients, config.Namespace)
+	})
+	runJob(func() ([]imageSpec, error) {
 		return imagesInDeployments(ctx, clients, config.Namespace)
+	})
+	runJob(func() ([]imageSpec, error) {
+		return imagesInStatefulSets(ctx, clients, config.Namespace)
 	})
 	go func() {
 		wg.Wait()
 		close(outputChannel)
 	}()
 
+	defer close(errChannel)
+
 	processedImageStrings := make(map[string]bool)
 	imagesInUse := make([]DockerImage, 0)
-	for image := range outputChannel {
-		if _, found := processedImageStrings[image]; found {
+	invalidImages := make([]string, 0)
+	for imageSpec := range outputChannel {
+		if _, found := processedImageStrings[imageSpec.Image]; found {
 			continue
 		}
-		imageParts := strings.SplitN(image, ":", 2)
+		if !imageSpec.IsValid {
+			println("Found invalid image: " + imageSpec.Image)
+			invalidImages = append(invalidImages, imageSpec.Image)
+			continue
+		}
+		imageParts := strings.SplitN(imageSpec.Image, ":", 2)
 		if len(imageParts) != 2 {
-			println("Found invalid image: " + image)
-			// We cannot skip these, or it might delete images that are in use.
-			// continue
+			println("Found invalid image (after splitting): " + imageSpec.Image)
+			invalidImages = append(invalidImages, imageSpec.Image)
+			continue
 		}
 		var registry *string
 		var name string
@@ -153,13 +198,15 @@ func FindImagesInUse(ctx context.Context, config Config) ([]DockerImage, error) 
 			Name:     name,
 			Tag:      imageParts[1],
 		})
-		processedImageStrings[image] = true
+		processedImageStrings[imageSpec.Image] = true
 	}
-
-	defer close(errChannel)
 
 	if len(errChannel) > 0 {
 		return nil, <-errChannel
+	}
+
+	if len(invalidImages) > 0 { // We cannot skip these, or it might delete images that are in use.
+		return nil, errors.New("Invalid images found: " + strings.Join(invalidImages, ", "))
 	}
 
 	return imagesInUse, nil
